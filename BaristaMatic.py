868505f32cbf44e28975a068be0bd61d45734d65
Stefan Knott <stkn5801@colorado.edu>
2015-11-12 11:45:55 -0700

    OrderedDict() update

--- BaristaMatic.py
+++ BaristaMatic.py
@@ -1,64 +1,76 @@
-import fileinput
+#Stefan Knott FA2015
+'''Barista Matic program which makes an assortment of drinks and handles inventory
+and drink selection accordingly
+'''
+
+import fileinput, collections
 
 def makeDrink(drinkName, drinkRecipes, ingredients):
 	recipe = drinkRecipes[drinkName]
-	for ingrName, units in recipe.iteritems():
-		if ingredients[ingrName]["stock"] >= units:
-			ingredients[ingrName]["stock"] -= units
-		else:
-			print "Not enough ingredients to make ", drinkName, " choose another"
+	
+	#Look
+	for ingrName, units in recipe.items():
+		if ingredients[ingrName]["stock"] < units:
+			print "Out of stock: ", drinkName
 			return 
 
+	#Make
 	print "Dispensing: ", drinkName
+	for ingrName, units in recipe.items():
+		ingredients[ingrName]["stock"] -= units
 
 def restock():
-	for name, info in ingredients.iteritems():
-		for attr, num in info.iteritems():
+	for name, info in ingredients.items():
+		for attr, num in info.items():
 			if attr == "stock" and num < 10:
 				info[attr] = 10
 
 def handleMenu(drinkRecipes, ingredients):
 	line = raw_input()
+
 	if line == 'q' or line == 'Q':
 		exit(1)
 	elif line == "r" or line == "R":
 		restock()
 	elif int(line) == 1:
-		makeDrink("Coffee", drinkRecipes, ingredients)
+		makeDrink("Caffe Americano", drinkRecipes, ingredients)
 	elif int(line) == 2:
-		makeDrink("Cappuccino", drinkRecipes, ingredients)
+		makeDrink("Caffe Latte", drinkRecipes, ingredients)
 	elif int(line) == 3:
-		makeDrink("DecafCoffee", drinkRecipes, ingredients)
+		makeDrink("Caffe Mocha", drinkRecipes, ingredients)
 	elif int(line) == 4:
-		makeDrink("CaffeLatte", drinkRecipes, ingredients)
+		makeDrink("Cappuccino", drinkRecipes, ingredients)
 	elif int(line) == 5:
-		makeDrink("CaffeAmericano", drinkRecipes, ingredients)
+		makeDrink("Coffee", drinkRecipes, ingredients)
 	elif int(line) == 6:
-		makeDrink("CaffeMocha", drinkRecipes, ingredients)
+		makeDrink("Decaf Coffee", drinkRecipes, ingredients)
+	else:
+		print "Invalid selection: ", line
+
 	baristaMatic(drinkRecipes, ingredients)
 
 def getDrinkInfo(iD, drinkName, recipe):
 	drinkCost = 0
-	for ingrName, units in recipe.iteritems():
+	for ingrName, units in recipe.items():
 		drinkCost += ingredients[ingrName]["cost"] *  units
 		if ingredients[ingrName]["stock"] <= 0:
 			#missing ingredients..
-			return str(iD) + ", " + drinkName + ", $" + str(drinkCost) + ",false"
+			return str(iD) + "," + drinkName + ",$" + str(drinkCost) + ",false"
 				
-	#have ingredients..
-	return str((str(iD) + ", " + drinkName + ", $" + str(drinkCost), ",true")).replace("(", "").replace("'", "").replace(")", "")
+	#have ingredients..odd how return string is formatted differently here than in for:if (hence the replace() chaining):
+	return str((str(iD) + "," + drinkName + ",$" + str(drinkCost), ",true")).replace("(", "").replace("'", "").replace(")", "")
 
 
 def printInventory(ingredients):
 	print "Inventory:"
-	for name, info in ingredients.iteritems():
-		for attr, num in info.iteritems():
+	for name, info in ingredients.items():
+		for attr, num in info.items():
 			if attr == "stock":
 				print name, ",", num
 
 def printMenu(drinkRecipes):
 	print "Menu:"
-	#could make optionMenu dict for dynamic number selection
+
 	iD = 1
 	for drinkName, recipe in drinkRecipes.iteritems():
 		print getDrinkInfo(iD, drinkName, recipe)
@@ -70,21 +82,24 @@ def baristaMatic(drinkRecipes, ingredients):
 	handleMenu(drinkRecipes, ingredients)
 
 if __name__ == '__main__':
-	ingredients = {"coffee" : {"cost" : 0.85, "stock" : 10}, 
-					"decafCoffe" : {"cost" : 0.85, "stock" : 10}, 
-					"sugar" : {"cost" : 0.20, "stock" : 10}, 
-					"cream" : {"cost" : 0.25, "stock" : 10}, 
-					"steamMilk" : {"cost" : 0.4, "stock" : 10},
-					"foamMilk" : {"cost" : .35, "stock" : 10},
-					"espresso" : {"cost" : 1.10, "stock" : 10},
-					"cocoa" : {"cost" : 1.10, "stock" : 10},
-					"whippedCream" : {"cost" : 1.00, "stock" : 10}}
-	
-	drinkRecipes = {"Coffee" : {"coffee" : 3, "sugar" : 1, "cream" : 1},
-				"DecafCoffee" : {"decafCoffe" : 3, "sugar" : 1, "cream" : 1},
-				"CaffeLatte" : {"espresso" : 2, "steamMilk" : 1},
-				"CaffeAmericano" : {"espresso" : 3},
-				"CaffeMocha" : {"espresso" : 1, "cocoa" : 1, "steamMilk" : 1, "whippedCream" : 1},
-				"Cappuccino" : {"espresso" : 2, "steamMilk" : 1, "foamMilk" : 1}}
+	ingredients = collections.OrderedDict()
+	drinkRecipes = collections.OrderedDict()
+
+	ingredients["Cocoa"] = {"cost" : 1.10, "stock" : 10}
+	ingredients["Coffee"] = {"cost" : 0.85, "stock" : 10}
+	ingredients["Cream"] = {"cost" : 0.25, "stock" : 10}
+	ingredients["Decaf Coffee"] = {"cost" : 0.85, "stock" : 10}
+	ingredients["Espresso"] = {"cost" : 1.10, "stock" : 10}
+	ingredients["Foamed Milk"] = {"cost" : .35, "stock" : 10}
+	ingredients["Steam Milk"] = {"cost" : 0.4, "stock" : 10}
+	ingredients["Sugar"] = {"cost" : 0.20, "stock" : 10}
+	ingredients["Whipped Cream"] = {"cost" : 1.00, "stock" : 10}
+
+	drinkRecipes["Caffe Americano"] = {"Espresso" : 3}
+	drinkRecipes["Caffe Latte"] = {"Espresso" : 2, "Cocoa" : 1, "Steam Milk" : 1, "Whipped Cream" : 1}
+	drinkRecipes["Caffe Mocha"] = {"Espresso" : 2, "Steam Milk" : 1}
+	drinkRecipes["Cappuccino"] = {"Espresso" : 2, "Steam Milk" : 1, "Foamed Milk" : 1}
+	drinkRecipes["Coffee"] = {"Coffee" : 3, "Sugar" : 1, "Cream" : 1}
+	drinkRecipes["Decaf Coffee"] = {"Decaf Coffee" : 3, "Sugar" : 1, "Cream" : 1}
 
 	baristaMatic(drinkRecipes, ingredients)
